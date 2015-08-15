fc702e7f54222e3062f721194d5234b28dc4f416
Adam Chappell <adamchapp@gmail.com>
2015-08-15 11:50:18 +0100

    App.listen express style

--- app.js
+++ app.js
@@ -64,7 +64,6 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
-app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1')
-
-var server = http.createServer(app).listen(port);
+var port = process.env.OPENSHIFT_NODEJS_PORT ||  process.env.OPENSHIFT_INTERNAL_PORT || 8080;  
+var ipaddr = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP;  
+app.listen(port, ipaddr);  
