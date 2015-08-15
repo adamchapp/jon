93d90c3fc0ad3c4c591ba2a47e5e588cd4538b85
Adam Chappell <adamchapp@gmail.com>
2015-08-15 11:42:02 +0100

    Remove debug config

--- app.js
+++ app.js
@@ -64,5 +64,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1')
 
-module.exports = app;
+var server = http.createServer(app).listen(port);
